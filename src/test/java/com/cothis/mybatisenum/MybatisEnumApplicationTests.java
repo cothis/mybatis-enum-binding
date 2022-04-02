832fa373a49296c8c6a1b3f16dddd4c42e2f9660
cothis <minho1030@naver.com>
2022-04-02 15:58:22 +0900

    feat: initial commit

---
+++ src/test/java/com/cothis/mybatisenum/MybatisEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cothis.mybatisenum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
